79cc1ecd3be36dc3110f4ee2ac18762315a26645
dave <dmh2000@gmail.com>
2023-08-28 17:29:43 -0700

    initial setup for vcan send/recv

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # can_iot
 sample canbus end-to-end
+
+## install vcan and can-utils
+
+### VCAN
+```bash
+#!/bin/bash
+# Load the kernel module.
+sudo modprobe vcan
+# Create the virtual CAN interface.
+sudo ip link add dev vcan0 type vcan
+# Bring the virtual CAN interface online.
+sudo ip link set up vcan0
+```
+
+### can-utils
+```bash
+
+# INSTALL
+sudo apt install can-utils
+
+# TEST
+# terminal 1
+candump -tz vcan0
+
+# terminal 2
+cansend vcan0 123#00FFAA5501020304
+```
+
+### C Test Programs
+
+./vcan/sender.c
+./vcan/receiver.c
+
+
+
---
+++ vcan/sender.c
@@ -0,0 +1,61 @@
+#include <linux/can.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <linux/if.h>
+// #include <net/if.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+
+int main(int argc,char *argv[]) {
+
+	int s;
+	int status;
+
+	s = socket(PF_CAN,SOCK_RAW,CAN_RAW);
+	if (s < 0) {
+		perror("socket");
+		return 1;
+	}
+
+	struct sockaddr_can addr;
+	memset(&addr,0,sizeof(addr));
+
+	struct ifreq ifr;
+ 
+	strcpy(ifr.ifr_name, "vcan0");
+ 
+	status = ioctl(s, SIOCGIFINDEX, &ifr);
+	if (status < 0) {
+		perror("ioctl");
+		return 1;
+	}
+ 
+	addr.can_family = AF_CAN;
+ 
+	addr.can_ifindex = ifr.ifr_ifindex;
+
+	status = bind(s, (struct sockaddr *)&addr, sizeof(addr));	
+	if (status < 0) {
+		perror("bind");
+		return 1;
+	} 
+
+	struct can_frame frame;
+
+	frame.can_id = 0x101;
+	frame.can_dlc = 2;
+	frame.data[0] = 0x41;
+	frame.data[1] = 0x42;
+
+	int bytes = write(s,&frame,sizeof(struct can_frame));
+	if (bytes <= 0) {
+		perror("write");
+		return 1;
+	}
+
+	close(s);
+
+	return 0;
+}
